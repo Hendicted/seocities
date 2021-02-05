d468580ae447d0fa8d7fd233bcc290b3660d6f0f
Hendicted <ur@nan.lol>
2021-02-05 16:45:22 +0100

    damn im stupid

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 $address = ""; #what the default address is (example: https://yourdomain.com/seocities will output https://yourdomain.com/seocities/sitename)
 $QL_SERVER = "localhost"; # dont change if you dont know what you are doing!
-$database = "seo"; # the database ur gonna use
+$database = ""; # the database ur gonna use
 $QL_PORT = "3306"; #dont change if you dont know what you are doing!
-$sqlusername = "hendicted";
-$sqlpassword = "ass";
+$sqlusername = "";
+$sqlpassword = "";
 $MAX_FPENTRIES = "10"; #how many sites should be featured on the front page
 
 function deleteDirectory($dir) {
